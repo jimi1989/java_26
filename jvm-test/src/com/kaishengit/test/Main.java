029a0bf95d180b1dbbf3775ec3e8b33d29acd01c
jimi_jin <jinjianghao@kaishengit.com>
2018-08-20 10:58:12 +0800

    jvm演示

--- jvm-test/src/com/kaishengit/test/Main.java
+++ jvm-test/src/com/kaishengit/test/Main.java
@@ -1,9 +1,42 @@
 package com.kaishengit.test;
 
+class SuperClass {
+    public static int value = 10;
+
+    static {
+        value = 100;
+        System.out.println("SuperClass static block");
+    }
+}
+
+class SubClass extends SuperClass {
+    public static int b = value;
+    static {
+        System.out.println("SubClass static block");
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        Runtime runtime = Runtime.getRuntime();
+
+        // 类加载器
+        ClassLoader loader = Main.class.getClassLoader();
+        System.out.println(loader);
+        System.out.println(loader.getParent());
+        System.out.println(loader.getParent().getParent());
+
+        // System.out.println(SubClass.b);
+
+        // System.out.println(SuperClass.value);
+
+        //SuperClass[] supers = new SuperClass[5];
+
+        // System.out.println(SubClass.value);
+
+
+
+        /*Runtime runtime = Runtime.getRuntime();
 
         //-Xms20m -Xmx20M -Xmn1m -XX:SurvivorRatio=2 -XX:+PrintGCDetails -XX:+UseSerialGC
         byte[] bytes = null;
@@ -15,7 +48,7 @@ public class Main {
             System.out.println("空闲内存： " + runtime.freeMemory());
             System.out.println("当前可用内存： " + runtime.totalMemory());
 
-        }
+        }*/
 
 
 
